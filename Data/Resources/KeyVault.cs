4f4f3620e1cb68f6b6aef89ca0e7802d631c6b4e
Alan Kelly <alan.kelly.london@gmail.com>
2021-01-20 17:02:14 +0000

    add keyvault, need to finish

---
+++ Data/Resources/KeyVault.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using blazorserver.Data;
+using Microsoft.Azure.Management.ContainerInstance.Fluent;
+using Microsoft.Azure.Management.Network.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+
+// https://docs.microsoft.com/en-us/rest/api/keyvault/vaults/get
+public class KeyVault : AzureResource
+{
+    public static string AzureType = "Microsoft.KeyVault/vaults";
+    public static string ApiVersion = "2019-09-01";
+    public static string TerraformType = "azurerm_key_vault";
+
+    public static new AzureResource FromJsonElement(JsonElement element)
+    {
+        KeyVault resource = new KeyVault();
+
+        // basic information
+        resource.ID = element.GetProperty("id").GetString();
+        resource.Name = element.GetProperty("name").GetString();
+        resource.Type = element.GetProperty("type").GetString();
+        resource.Location = element.GetProperty("location").GetString();
+
+        // TODO: resource specific information
+
+        return resource;
+    }
+
+    public override List<string> GetReferences()
+    {
+        List<string> refs = new List<string>();
+
+        // TODO: find any external references - maybe network profile or image server (ACR)
+
+        return refs;
+    }
+
+    public override string Emit()
+    {
+        StringBuilder builder = new StringBuilder();
+
+        builder.Append($"resource \"{TerraformType}\" \"{TerraformNameFromResourceName(Name)}\" {{\r\n");
+        builder.Append($"  resource_group_name = {ResourceGroupName}\r\n");
+        builder.Append($"  location            = {Location}\r\n");
+        builder.Append($"}}\r\n");
+        builder.Append("\r\n");
+
+        return builder.ToString();
+    }
+}
