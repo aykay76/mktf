5a2f5876957c865d3ade613577c6edf4e73d13ef
Alan Kelly <alan.kelly.london@gmail.com>
2021-01-27 20:24:33 +0000

    add storage profile

--- Data/Resources/PostgreSQLServer.cs
+++ Data/Resources/PostgreSQLServer.cs
@@ -21,6 +21,10 @@ namespace blazorserver.Data.Resources
         public string AdministratorLoginPassword { get; set; }
         public string SslEnforcement { get; set; }
         public string MinimumTlsVersion { get; set; }
+        public int StorageMb { get; set; }
+        public int BackupRetentionDays { get; set; }
+        public string GeoRedundantBackup { get; set; }
+        public string StorageAutogrow { get; set; }
 
         public static new AzureResource FromJsonElement(JsonElement element)
         {
@@ -42,6 +46,12 @@ namespace blazorserver.Data.Resources
             resource.SslEnforcement = properties.GetProperty("sslEnforcement").GetString();
             resource.MinimumTlsVersion = properties.GetProperty("minimalTlsVersion").GetString();
 
+            JsonElement storageProfile = properties.GetProperty("storageProfile");
+            resource.StorageMb = storageProfile.GetProperty("storageMB").GetInt32();
+            resource.BackupRetentionDays = storageProfile.GetProperty("backupRetentionDays").GetInt32();
+            resource.GeoRedundantBackup = storageProfile.GetProperty("geoRedundantBackup").GetString();
+            resource.StorageAutogrow = storageProfile.GetProperty("storageAutogrow").GetString();
+
             return resource;
         }
 
@@ -62,6 +72,12 @@ namespace blazorserver.Data.Resources
             builder.AppendLine();
             builder.AppendLine($"  ssl_enforcement_enabled = \"{SslEnforcement}\"");
             builder.AppendLine($"  ssl_minimal_tls_version_enforced = \"{MinimumTlsVersion}\"");
+            builder.AppendLine();
+            builder.AppendLine($"  storage_mb                   = {StorageMb}");
+            builder.AppendLine($"  backup_retention_days        = {BackupRetentionDays}");
+            builder.AppendLine($"  geo_redundant_backup_enabled = {GeoRedundantBackup == "Enabled"}");
+            builder.AppendLine($"  auto_grow_enabled            = {StorageAutogrow == "Enabled"}");
+
             builder.AppendLine($"}}");
             builder.AppendLine();
 
