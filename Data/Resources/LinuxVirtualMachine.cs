e6dfaa8fac82599566aa238868523608d02acfec
Alan Kelly <alan.kelly.london@gmail.com>
2021-01-30 10:41:16 +0000

    get ready for disk and virtual machines

---
+++ Data/Resources/LinuxVirtualMachine.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using blazorserver.Data;
+using Microsoft.Azure.Management.ContainerInstance.Fluent;
+using Microsoft.Azure.Management.Network.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+
+// https://docs.microsoft.com/en-us/rest/api/compute/virtualmachines/get
+// https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine
+namespace blazorserver.Data.Resources
+{
+    public class LinuxVirtualMachine : AzureResource
+    {
+        public static string AzureType = "Microsoft.Compute/virtualMachines";
+        public static string ApiVersion = "2020-06-01";
+        public static string TerraformType = "azurerm_linux_virtual_machine";
+
+        // TODO: do
+
+        public static new AzureResource FromJsonElement(JsonElement element)
+        {
+            LinuxVirtualMachine resource = new LinuxVirtualMachine();
+            resource.Description = element;
+
+            // basic information
+            resource.ID = element.GetProperty("id").GetString();
+            resource.Name = element.GetProperty("name").GetString();
+            resource.Type = element.GetProperty("type").GetString();
+            resource.Location = element.GetProperty("location").GetString();
+
+
+
+            return resource;
+        }
+
+        public override string Emit()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine($"resource \"{TerraformType}\" \"{TerraformNameFromResourceName(Name)}\" {{");
+            builder.AppendLine($"  resource_group_name = \"{ResourceGroupName}\"");
+            builder.AppendLine($"  location            = \"{Location}\"");
+            builder.AppendLine();
+            builder.AppendLine($"  name     = \"{Name}\"");
+
+            builder.AppendLine($"}}");
+            builder.AppendLine();
+
+            return builder.ToString();
+        }
+    }
+}
